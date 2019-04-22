76433b26121f850f69e7771b2b16579b71a137a6
Rafael Barreto <rafaelbarretorb@gmail.com>
2019-04-22 01:52:49 -0300

    Add a few methods

--- include/rrt_star_global_planner/RRTstar_ros.h
+++ include/rrt_star_global_planner/RRTstar_ros.h
@@ -10,15 +10,17 @@
 #include <base_local_planner/world_model.h>
 #include <base_local_planner/costmap_model.h>
 
+#include <cmath>
+
 using std::string;
 
 #ifndef RRTSTAR_ROS_CPP
 #define RRTSTAR_ROS_CPP
 
 struct Node {
-	std::vector<float> point;
-	float parent;
-  struct Node* parent;
+	float x;
+  float y;
+	int parent_index;
   float cost;
 }; 
 
@@ -27,18 +29,172 @@ namespace RRTstar_planner
 
   class RRTstarPlannerROS : public nav_core::BaseGlobalPlanner 
   {
-      public :
-        float XDIM;
-        float YDIM;
+    public:
+      float XDIM;
+      float YDIM;
 
-        RRTstarPlannerROS();
-        RRTstarPlannerROS(std::string name, costmap_2d::Costmap2DROS∗ costmap_ros);
+      RRTstarPlannerROS();
+      RRTstarPlannerROS(std::string name, costmap_2d::Costmap2DROS∗ costmap_ros);
+      /** overridden classes from interface nav_core::BaseGlobalPlanner */
+      void initialize(std::string name, costmap_2d::Costmap2DROS∗ costmap_ros);
+      bool makePlan(const geometry_msgs::PoseStamped& start,
+                    const geometry_msgs::PoseStamped& goal,
+                    std::vector<geometry_msgs::PoseStamped>& plan);
 
-        /** overridden classes from interface nav_core::BaseGlobalPlanner */
-        void initialize(std::string name, costmap_2d::Costmap2DROS∗ costmap_ros);
-        bool makePlan(const geometry_msgs::PoseStamped& start,
-                      const geometry_msgs::PoseStamped& goal,
-                      std::vector<geometry_msgs::PoseStamped>& plan);
+    protected:
+
+        /**
+         * @brief Store a copy of the current costmap in \a costmap.  Called by makePlan.
+         */
+        costmap_2d::Costmap2D* costmap_;
+        std::string frame_id_;
+        ros::Publisher plan_pub_;
+        bool initialized_, allow_unknown_;
+
+    private:
+        void mapToWorld(double mx, double my, double& wx, double& wy);
+bool worldToMap(double wx, double wy, double& mx, double& my);
   };
+};
+#endif
+
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h> 
+#include <string>
+
+/** include ros libraries**********************/
+#include <ros/ros.h>
+
+#include <actionlib/client/simple_action_client.h>
+#include <move_base_msgs/MoveBaseAction.h>
+
+#include <geometry_msgs/Twist.h>
+#include <geometry_msgs/PoseStamped.h>
+#include <geometry_msgs/PoseWithCovarianceStamped.h>
+#include <move_base_msgs/MoveBaseGoal.h>
+#include <move_base_msgs/MoveBaseActionGoal.h>
+
+#include "sensor_msgs/LaserScan.h"
+#include "sensor_msgs/PointCloud2.h"
+
+#include <nav_msgs/Odometry.h>
+#include <nav_msgs/OccupancyGrid.h>
+#include <nav_msgs/Path.h>
+#include <nav_msgs/GetPlan.h>
+
+#include <tf/tf.h>
+#include <tf/transform_datatypes.h>
+#include <tf/transform_listener.h>
+/** ********************************************/ 
+
+#include <boost/foreach.hpp>
+//#define forEach BOOST_FOREACH
+
+/** for global path planner interface */
+#include <costmap_2d/costmap_2d_ros.h>
+#include <costmap_2d/costmap_2d.h>
+#include <nav_core/base_global_planner.h>
+
+#include <geometry_msgs/PoseStamped.h>
+#include <angles/angles.h>
+
+//#include <pcl_conversions/pcl_conversions.h>
+#include <base_local_planner/world_model.h>
+#include <base_local_planner/costmap_model.h>
+
+#include <set>
+using namespace std;
+using std::string;
+
+#ifndef RASTAR_ROS_CPP
+#define RASTAR_ROS_CPP
+
+/**
+ * @struct cells
+ * @brief A struct that represents a cell and its fCost.
+ */
+struct cells {
+	int currentCell;
+	float fCost;
+
+};      
+
+namespace RAstar_planner {
+  
+class RAstarPlannerROS : public nav_core::BaseGlobalPlanner {
+public:
+  
+  RAstarPlannerROS (ros::NodeHandle &); //this constructor is may be not needed
+  RAstarPlannerROS ();
+  RAstarPlannerROS(std::string name, costmap_2d::Costmap2DROS* costmap_ros);
+  
+  ros::NodeHandle ROSNodeHandle;
+  
+  /** overriden classes from interface nav_core::BaseGlobalPlanner **/
+  void initialize(std::string name, costmap_2d::Costmap2DROS* costmap_ros);
+  bool makePlan(const geometry_msgs::PoseStamped& start, 
+		const geometry_msgs::PoseStamped& goal, 
+		std::vector<geometry_msgs::PoseStamped>& plan
+	       );
+ 
+
+  void getCorrdinate (float& x, float& y);
+  int convertToCellIndex (float x, float y);
+  void convertToCoordinate(int index, float& x, float& y);
+  bool isCellInsideMap(float x, float y);
+  void mapToWorld(double mx, double my, double& wx, double& wy);
+  vector<int> RAstarPlanner(int startCell, int goalCell);
+  vector<int> findPath(int startCell, int goalCell, float g_score[]);
+  vector<int> constructPath(int startCell, int goalCell, float g_score[]);
+  float calculateHCost(int cellID, int goalCell){
+  int x1=getCellRowID(goalCell);
+  int y1=getCellColID(goalCell);
+  int x2=getCellRowID(cellID);
+  int y2=getCellColID(cellID);
+    return abs(x1-x2)+abs(y1-y2);
+	//return min(abs(x1-x2),abs(y1-y2))*sqrt(2) + max(abs(x1-x2),abs(y1-y2))-min(abs(x1-x2),abs(y1-y2));
+  }
+  void addNeighborCellToOpenList(multiset<cells> & OPL, int neighborCell, int goalCell, float g_score[]);
+  vector <int> findFreeNeighborCell (int CellID);
+  bool isStartAndGoalCellsValid(int startCell,int goalCell); 
+  float getMoveCost(int CellID1, int CellID2);
+  float getMoveCost(int i1, int j1, int i2, int j2);
+  bool isFree(int CellID); //returns true if the cell is Free
+  bool isFree(int i, int j); 
+
+  int getCellIndex(int i,int j) //get the index of the cell to be used in Path
+  {
+   return (i*width)+j;  
+  }
+  int getCellRowID(int index)//get the row ID from cell index
+  {
+     return index/width;
+  }
+  int getCellColID(int index)//get colunm ID from cell index
+  {
+    return index%width;
+  }
+
+  float originX;
+  float originY;
+  float resolution;
+
+  costmap_2d::Costmap2DROS* costmap_ros_;
+  double step_size_, min_dist_from_robot_;
+  costmap_2d::Costmap2D* costmap_;
+  //base_local_planner::WorldModel* world_model_;
+  bool initialized_;
+  int width;
+  int height;
+};
+
 };
 #endif
--- src/RRTstar_ros.cpp
+++ src/RRTstar_ros.cpp
@@ -1,4 +1,7 @@
+/*
+  planner_core.h
 
+*/
 
 #include <rrt_star_global_planner/RRTstar_ros.h>
 
@@ -47,7 +50,7 @@ namespace RRTstar_planner
         }
       }
 
-      ROS_INFO("RAstar planner initialized successfully");
+      ROS_INFO("RRT* planner initialized successfully");
       initialized_ = true;
     }
     else
@@ -58,14 +61,49 @@ namespace RRTstar_planner
   {
     float x_dim = this -> XDIM;
     float y_dim = this -> YDIM;
+
+    std::vector<Node> nodes;
+    std::vector<float> x_new(2);
+    Node x_nearest;
+    Node x_new;
     while (nodes.size() < this -> max_num_nodes)
     {
       bool found_next = false;
       while (found_next == false)
       {
-        std::vector<float> x_rand = this -> sampleFree(x_dim, y_dim);
-        Node x_nearest = this -> getNearest(nodes, x_rand);
-        x_new = this -> steer(x_nearest.point, x_rand)
+        x_rand = this -> sampleFree(x_dim, y_dim);
+
+        GetNearestListIndex
+        // TODO
+        x_nearest_id = this -> getNearest(nodes, x_rand);
+
+        
+        x_new = this -> steer(x_nearest.point, x_rand);
+        if (this->obstacle_free(x_nearest, x_new))
+        {
+          
+          new
+          parent_node = x_nearest;
+
+          
+          Node newnode = {x_new.x, x_new.y, x_nearest_id, 0.0};
+
+          nodes.push_back(newnode);
+
+        // Python
+                    parent_node = x_nearest
+
+                    newnode = Node(x_new, parent_node)
+
+                    [newnode, x_nearest] = self.choose_parent(parent_node, newnode, nodes)
+                    nodes.append(newnode)
+                    nodes = self.rewire(nodes,newnode)
+
+                    nodes.append(newnode)
+             
+                    foundNext = True
+        }
+
       }
     }
 
@@ -110,6 +148,11 @@ namespace RRTstar_planner
 
   }
 
+  float RRTstarPlannerROS::distance(px1, py1, px2, py2)
+  {
+    float dist = sqrt((px1 - px2)*(px1 - px2) + (py1 - py2)*(py1 - py2));
+    return dist;
+  }
 
   std::vector<float> RRTstarPlannerROS::sampleFree(float x_dim, float y_dim)
   {
@@ -120,3 +163,86 @@ namespace RRTstar_planner
     std::vector<float> x_rand = {x,y};
     return x_rand; 
   }
+
+  std::vector<int> worldToMap()
+  {
+    int cell_column = int((x - originX) / resolution);
+    return 
+  }
+
+
+void RRTstarPlannerROS::mapToWorld(double mx, double my, double& wx, double& wy) {
+    wx = costmap_->getOriginX() + (mx+convert_offset_) * costmap_->getResolution();
+    wy = costmap_->getOriginY() + (my+convert_offset_) * costmap_->getResolution();
+}
+
+bool RRTstarPlannerROS::worldToMap(double wx, double wy, double& mx, double& my) {
+    double origin_x = costmap_->getOriginX(), origin_y = costmap_->getOriginY();
+    double resolution = costmap_->getResolution();
+
+    if (wx < origin_x || wy < origin_y)
+        return false;
+
+    mx = (wx - origin_x) / resolution - convert_offset_;
+    my = (wy - origin_y) / resolution - convert_offset_;
+
+    if (mx < costmap_->getSizeInCellsX() && my < costmap_->getSizeInCellsY())
+        return true;
+
+    return false;
+}
+
+      # get the cell coord of the center point of the robot
+    def world_to_map(self, x, y):
+        cell_column = int((x - self.MAP.info.origin.position.x) / self.MAP.info.resolution)
+cell_row = int((y - self.MAP.info.origin.position.y) / self.MAP.info.resolution)
+
+
+  //check if point collides with the obstacle
+  bool RRTstarPlannerROS::collision(wx, wy)
+  {
+    int mx, my;
+    worldToMap(double wx, double wy, double& mx, double& my);
+    
+
+  }
+      
+
+        # TODO: get a point and transform into cell grid
+
+        # TODO: verify if thi
+
+        cell_row, cell_column = self.world_to_map(p[0], p[1])
+
+        if cell_column < 0:
+            return True
+        elif cell_row < 0:
+            return True
+        elif cell_row - 1 < 0:
+            return True
+        elif cell_column -1 < 0:
+            return True
+        elif cell_column >= self.grid.shape[1]:
+            return True
+        elif cell_row >= self.grid.shape[0]:
+            return True
+        elif cell_column+1 >= self.grid.shape[1]:
+            return True
+        elif cell_row +1>= self.grid.shape[0]:
+            return True
+        elif self.grid[cell_row][cell_column] == 1:
+            return True
+        elif self.grid[cell_row+1][cell_column+1] == 1:
+            return True
+        elif self.grid[cell_row][cell_column+1] == 1:
+            return True
+        elif self.grid[cell_row+1][cell_column] == 1:
+            return True
+        elif self.grid[cell_row-1][cell_column] == 1:
+            return True  
+        elif self.grid[cell_row][cell_column-1] == 1:
+            return True   
+        elif self.grid[cell_row-1][cell_column-1] == 1:
+            return True
+        else:
+            return False
