dc782875b93ecdf123ab1ef4642cabe7af3f7718
rafaelbarretorb <rafaelbarretorb@gmail.com>
2021-11-02 19:37:21 -0300

    angles lib is not a dependency

--- include/rrt_star_global_planner/rrt_star_planner.hpp
+++ include/rrt_star_global_planner/rrt_star_planner.hpp
@@ -15,7 +15,6 @@
 #include <costmap_2d/costmap_2d.h>
 #include <nav_core/base_global_planner.h>
 #include <geometry_msgs/PoseStamped.h>
-#include <angles/angles.h>
 #include <base_local_planner/world_model.h>
 #include <base_local_planner/costmap_model.h>
 
