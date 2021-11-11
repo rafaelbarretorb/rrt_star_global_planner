2f34d0101984a250df95a802b217d4b4f7fd5f95
rafaelbarretorb <rafaelbarretorb@gmail.com>
2021-11-10 22:39:18 -0300

    add tests to check methods chooseParent and rewire

--- include/rrt_star_global_planner/rrt_star.hpp
+++ include/rrt_star_global_planner/rrt_star.hpp
@@ -15,6 +15,10 @@
 
 namespace rrt_star_global_planner {
 
+inline float euclideanDistance2D(float x1, float y1, float x2, float y2) {
+  return std::hypot((x1 - x2), (y1 - y2));
+}
+
 class RRTStar {
  public:
   RRTStar(const std::pair<float, float> &start_point,
@@ -44,11 +48,90 @@ class RRTStar {
     return true;
   }
 
+  void createNewNode(float x, float y, int node_nearest_id) {
+    Node new_node(x, y, node_count_, node_nearest_id);
+    nodes_.push_back(new_node);
+
+    if(node_nearest_id != -1) {
+      // Optimize
+      chooseParent(node_nearest_id);  // Select the best parent
+      rewire();  // rewire
+    }
+
+    node_count_++;
+  }
+
+  void chooseParent(int node_nearest_id) {
+    float cost_new_node;
+    float cost_other_parent;
+    float nodes_dist;
+
+    Node parent_node = nodes_[node_nearest_id];
+
+    Node &new_node = nodes_.back();
+
+    for(const auto &node : nodes_) {
+      if (node.node_id == new_node.node_id) break;
+      // distance between node and new_node
+      nodes_dist = euclideanDistance2D(node.x, node.y, new_node.x, new_node.y);
+
+      if (nodes_dist < radius_) {
+        // current cost of new_node
+        cost_new_node = parent_node.cost + euclideanDistance2D(parent_node.x, parent_node.y, new_node.x, new_node.y);
+        
+        // cost if the parent is node
+        cost_other_parent = node.cost + nodes_dist;
+
+        if (cost_other_parent < cost_new_node) {
+          if (obstacleFree(node, new_node.x, new_node.y)) {
+            parent_node = node;
+          }
+        }
+      }
+    }
+
+    // Update new_node cost and its new parent
+    new_node.cost = parent_node.cost + euclideanDistance2D(parent_node.x, parent_node.y, new_node.x, new_node.y);
+    new_node.parent_id = parent_node.node_id;
+  }
+
+  void rewire() {
+    float nodes_dist;
+    float cost_node;
+
+    Node new_node = nodes_.back();
+
+    for(auto &node : nodes_) {
+      // distance between node and new_node
+      nodes_dist = euclideanDistance2D(node.x, node.y, new_node.x, new_node.y);
+
+      // check if node is already the parent and if node is near for optimization
+      if(node != nodes_[new_node.parent_id] && nodes_dist < radius_) {
+        // cost if the parent of node is new_node
+        cost_node = new_node.cost + euclideanDistance2D(node.x, node.y, new_node.x, new_node.y);
+
+        if(cost_node < node.cost && obstacleFree(node, new_node.x, new_node.y)) {
+          // update the new parent of node and its new cost
+          node.parent_id = new_node.node_id;
+          node.cost = cost_node;
+        }
+      }
+    }
+  }
+
+  const std::vector<Node> &getNodes() {
+    return nodes_;
+  }
+
  private:
   std::pair<float, float> start_point_;
   std::pair<float, float> goal_point_;
   std::vector<Node> nodes_;
   RandomDoubleGenerator random_double_;
+  int node_count_{0};
+  float map_width_;
+  float map_height_;
+  double radius_{1.3};
 };
 
 }  // namespace rrt_star_global_planner
--- test/src/test_rrt_star.cpp
+++ test/src/test_rrt_star.cpp
@@ -6,22 +6,33 @@
 #include <utility>
 
 #include "rrt_star_global_planner/rrt_star.hpp"
+#include "rrt_star_global_planner/node.hpp"
 
 using rrt_star_global_planner::RRTStar;
-
+using rrt_star_global_planner::Node;
 
 int g_argc;
 char** g_argv;
 
 class RRTStarTest : public ::testing::Test {
  public:
-  RRTStarTest() {}
+  RRTStarTest() {
+    
+  }
   virtual ~RRTStarTest() {}
 
   void SetUp() override {
     std::pair<float, float> start = {0., 0.};
     std::pair<float, float> goal = {5., 5.};
     rrt_star = new RRTStar(start, goal, nullptr);
+
+    rrt_star->createNewNode(0.0, 0.0, -1);  // 0
+    rrt_star->createNewNode(-0.5, 1.5, 0);  // 1
+    rrt_star->createNewNode(1.0, 3.0, 1);  // 2
+    rrt_star->createNewNode(3.0, 3.8, 2);  // 3 - nearest of new node
+    rrt_star->createNewNode(3.5, 7.0, 3);  // 4
+    rrt_star->createNewNode(2.2, 2.0, 0);  // 5 - low cost new node
+    rrt_star->createNewNode(3.0, 3.0, 3);  // 6 - new node
   }
 
   void TearDown() override {
@@ -30,12 +41,27 @@ class RRTStarTest : public ::testing::Test {
   }
 
   RRTStar *rrt_star{nullptr};
+
+
 };
 
 // just test the test, remove it
-TEST_F(RRTStarTest, getRandomDoubles) {
-  ASSERT_NE(1, 2);  // false
+TEST_F(RRTStarTest, ChooseTheBestParentForTheNewNode) {
+  const std::vector<Node> &nodes = rrt_star->getNodes();
+  EXPECT_EQ(nodes.back().parent_id, 5);
 }
+
+TEST_F(RRTStarTest, RewireNodesAroundNewNode) {
+  const std::vector<Node> &nodes = rrt_star->getNodes(); 
+  EXPECT_EQ(nodes[3].parent_id, 6);
+}
+
+TEST_F(RRTStarTest, DISABLED_NewNodeFinalCost) {
+}
+
+TEST_F(RRTStarTest, DISABLED_NearestNodeFinalCost) {
+}
+
 
 
 int main(int argc, char** argv) {
