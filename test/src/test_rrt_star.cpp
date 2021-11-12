087de9360b7333b1dec86e65ebc5d1e11bf9d961
rafaelbarretorb <rafaelbarretorb@gmail.com>
2021-11-11 22:34:56 -0300

    tests pass to verity best parent search and rewire

--- test/src/test_rrt_star.cpp
+++ test/src/test_rrt_star.cpp
@@ -26,12 +26,12 @@ class RRTStarTest : public ::testing::Test {
     std::pair<float, float> goal = {5., 5.};
     rrt_star = new RRTStar(start, goal, nullptr);
 
-    rrt_star->createNewNode(0.0, 0.0, -1);  // 0
+    rrt_star->createNewNode(0.5, 0.5, -1);  // 0
     rrt_star->createNewNode(-0.5, 1.5, 0);  // 1
     rrt_star->createNewNode(1.0, 3.0, 1);  // 2
     rrt_star->createNewNode(3.0, 3.8, 2);  // 3 - nearest of new node
     rrt_star->createNewNode(3.5, 7.0, 3);  // 4
-    rrt_star->createNewNode(2.2, 2.0, 0);  // 5 - low cost new node
+    rrt_star->createNewNode(2.2, 2.0, 0);  // 5 - low cost near of new node
     rrt_star->createNewNode(3.0, 3.0, 3);  // 6 - new node
   }
 
@@ -45,7 +45,6 @@ class RRTStarTest : public ::testing::Test {
 
 };
 
-// just test the test, remove it
 TEST_F(RRTStarTest, ChooseTheBestParentForTheNewNode) {
   const std::vector<Node> &nodes = rrt_star->getNodes();
   EXPECT_EQ(nodes.back().parent_id, 5);
@@ -56,12 +55,15 @@ TEST_F(RRTStarTest, RewireNodesAroundNewNode) {
   EXPECT_EQ(nodes[3].parent_id, 6);
 }
 
-TEST_F(RRTStarTest, DISABLED_NewNodeFinalCost) {
+TEST_F(RRTStarTest, NewNodeFinalCost) {
+  const std::vector<Node> &nodes = rrt_star->getNodes();
+  EXPECT_NEAR(nodes.back().cost, 3.5478, 0.001);
 }
 
-TEST_F(RRTStarTest, DISABLED_NearestNodeFinalCost) {
+TEST_F(RRTStarTest, NearestNodeFinalCost) {
+  const std::vector<Node> &nodes = rrt_star->getNodes();
+  EXPECT_NEAR(nodes[3].cost, 4.3478, 0.001);
 }
-
 
 
 int main(int argc, char** argv) {
