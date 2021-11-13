449269074951ab34b39b5134a71a9356d8e4f8a7
rafaelbarretorb <rafaelbarretorb@gmail.com>
2021-11-13 20:43:03 -0300

    move distance function

--- include/rrt_star_global_planner/node.hpp
+++ include/rrt_star_global_planner/node.hpp
@@ -3,6 +3,10 @@
 
 namespace rrt_star_global_planner {
 
+inline float euclideanDistance2D(float x1, float y1, float x2, float y2) {
+  return std::hypot((x1 - x2), (y1 - y2));
+}
+
 struct Node {
 	float x;
   float y;
